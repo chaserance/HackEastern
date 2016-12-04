446ea0fc1f13a57dd3a6225e9d4d0b973e1130ee
jiansong he <jiansonghe@live.cn>
2016-12-04 04:40:09 -0500

    Add files via upload

--- ParseHTML.java
+++ ParseHTML.java
@@ -1,6 +1,31 @@
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
+
+import java.util.*;
+
 public class ParseHTML
 {
-	public void ParseHTML()
+	ArrayList<HtmlPage> pagesToParse;
+	ArrayList<Integer> numberOfSeatsAvailable;
+	ArrayList<Integer> numberOfWaitlistAvailable;
+	Search mySearch;
+	WebClient myClient;
+	
+	
+	public ParseHTML() throws Exception
+	{
+		mySearch = new Search("COSC","511");
+		myClient = mySearch.myClient;
+		pagesToParse = mySearch.pages;
+		
+		for(HtmlPage p: pagesToParse)
+		{
+			List<HtmlElement> tags=p.getDocumentElement().getElementsByAttribute("th", "CLASS", "ddtitle");
+			System.out.println(p.asText());
+		}
+	}
+	
+	public void initClient()
 	{
 		
 	}
