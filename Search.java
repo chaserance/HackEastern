95339b6e14e8d87a30e1689af227eef54e9ecf6e
jiansong he <jiansonghe@live.cn>
2016-12-03 22:42:21 -0500

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,6 +1,7 @@
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -14,8 +15,10 @@ public class Search
 	final String PASSWORD = "910813";
 	String classCode;
 	String classNum;
-	String xmlResult;
-	String textResult;
+//	String xmlResult;
+//	String textResult;
+	
+	ArrayList<HtmlPage> contents;
 	
 	HtmlPage currentPage;
 	//String banner = "http://catalog.emich.edu/content.php?catoid=20&navoid=4199";
@@ -55,8 +58,8 @@ public class Search
 		HtmlAnchor link = (HtmlAnchor) currentPage.getFormByName("course_search").getElementsByTagName("a");
 		System.out.println(link.asXml());
 		*/
-		this.textResult = currentPage.asText();
-		this.xmlResult = currentPage.asXml();
+		//this.textResult = currentPage.asText();
+		//this.xmlResult = currentPage.asXml();
 		List<HtmlElement> tags=currentPage.getDocumentElement().getElementsByAttribute("th", "CLASS", "ddtitle");
 		ArrayList<String> links = new ArrayList<String>();
 		for(HtmlElement e : tags)
@@ -65,16 +68,24 @@ public class Search
 			int start = current.indexOf("a href=");
 			int end = current.indexOf("\"",start+9);
 			String result = current.substring(start+8,end);
-			System.out.println(result);
 			links.add(result);
-		}
+		}		
+		
+		getPageContent(links, currentPage);
 		System.out.println("done");
 		myClient.close();
 	}
 	
-	public void getPageContent(ArrayList<String> links)
+	private void getPageContent(ArrayList<String> patterns, HtmlPage page)
 	{
-		String url ="https://bannerweb.emich.edu/";
-		ArrayList<String> 
+		//String url ="https://bannerweb.emich.edu/";
+		ArrayList<HtmlAnchor> anchors = new ArrayList<HtmlAnchor>();
+		for(String s : patterns)
+		{
+			System.out.println(s);
+			HtmlAnchor anchor = page.getAnchorByHref(s);
+			System.out.println(anchor.asText());
+		}
 	}
 }
+
