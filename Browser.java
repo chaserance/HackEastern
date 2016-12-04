b37eb08ebb867d555173950ac34e320dcbbb63f1
Chushu Liu <chushul512@gmail.com>
2016-12-03 23:00:49 -0500

    Add files via upload

---
+++ Browser.java
@@ -0,0 +1,13 @@
+package edu.emich.hackeastern.Utility;
+
+public interface Browser {
+	/**
+	 * Return true if EID/PIN is correct.
+	 * @param EID
+	 * @param PIN
+	 * @return 
+	 */
+	public boolean logSuccessful(String EID, String PIN);
+	
+	
+}
